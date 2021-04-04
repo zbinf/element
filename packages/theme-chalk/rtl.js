abc736f890512fa2771d950f840021cbe1127a7e
zbinf <7837133+zbinf@user.noreply.gitee.com>
2021-04-04 22:54:49 +0800

    update

--- packages/theme-chalk/rtl.js
+++ packages/theme-chalk/rtl.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 
 const fileUrl = './lib/test.css'; // 目标文件
-const filetCopyUrl = './lib/testCopy.css'; // 目标文件副本
-const resultFile = './lib/testResult.scss';
+const filetCopyUrl = './lib/tagCopy.css'; // 目标文件副本
+const resultFile = './lib/testResult.scss'; // 过滤后的文件
 
 // 过滤的目标字段
 const filterTexts = [
@@ -11,12 +11,20 @@ const filterTexts = [
   'right',
   // 'rotateY(0)',
 ];
+let resultData = ''; // 写入文件的数据
+let parent = ''; //.test{ 存储父级字段
+let content = ''; // left: 1px; 存储确定保留的内容
+let cachContent = ''; // widht:1px; 存储待检测内容
+
 
 const getFile = () => {
-  fs.readFile(filetCopyUrl, (err, data) => {
+  resultData = '';
+  resetData();
+  fs.readFile(filetCopyUrl, 'utf-8', (err, data) => {
     if (err) {
-      fs.readFile(fileUrl, function (err, data) {
+      fs.readFile(fileUrl, 'utf-8', (err, data) => {
         if (err) return console.error(err);
+
         writeFile(filetCopyUrl, data, (err) => {
           // 写入成功
           if (!err) filterFile(data);
@@ -24,7 +32,6 @@ const getFile = () => {
       });
       return;
     }
-    console.log('读取文件', data);
     filterFile(data);
   });
 }
@@ -39,10 +46,101 @@ const writeFile = (path, data, callback) => {
 
 // 过滤文件
 const filterFile = (data) => {
-  console.log('data--', data);
   for (let i = 0, len = data.length; i < len; i++) {
     const el = data[i];
-    console.log('el：',i, el);
+    console.log('进度：',i, '/',len);
+
+    if (el == '}') {
+      // console.log('} --', cachContent, content);
+      if (!cachContent && content) {
+        // 有保留数据，赋值给resultData， 并重置
+        content += el;
+        resultData += parent;
+        resultData += content;
+      }
+
+      if (cachContent) {
+        // 还有带检测数据
+        const isHas = filterData(cachContent, 1);
+        if (isHas) content += cachContent;
+        if (content) {
+          content += el;
+          resultData += parent;
+          resultData += content;
+        }
+      }
+
+      resetData();
+      continue;
+    }
+
+    if (parent.indexOf('{') >= 0) {
+      if (el == ';') {
+        // 检测cachContent
+        const isHas = filterData(cachContent,2);
+        if (isHas) {
+          content += cachContent;
+          content += el;
+        }
+        cachContent = '';
+        continue;
+      }
+
+      cachContent += el;
+      // console.log('cachContent-', cachContent);
+      continue;
+    }
+
+    // 放入父级容器
+    parent += el;
+    // console.log('parent-', parent);
   }
+
+  console.log('遍历结束--resultData:', resultData);
+
+  // 遍历结束
+  writeFile(resultFile, resultData, (err) => {
+    // 写入成功
+    if (!err) console.log('完成');
+  })
+}
+
+
+
+// 检测是否包含方向性内容 
+// data: width:1px;
+const filterData = (data, type) => {
+  if (!data) return false;
+  let isHas = false;
+  console.log('带检测data--', type, data);
+  for (let i = 0; i < filterTexts.length; i++) {
+    const filterText = filterTexts[i];
+    if (data.indexOf(filterText) >= 0) {
+      isHas = true;
+      break;
+    }
+  }
+  // const special = ['margin', 'padding', 'border-width'];
+  // 检测是否包含 margin: 0px 2px 0 1px; 这种格式
+  if (!isHas && (data.indexOf('margin') >= 0 || data.indexOf('padding') >= 0 || data.indexOf('border-width') >= 0)) {
+    const dataArr = data.split(':');
+    dataArr[1] = dataArr[1].trim(); // 去掉前后空格
+    const valArr = dataArr[1].split(' ');
+
+    if (valArr.length > 3 && valArr[1] != valArr[3]) {
+      isHas = true;
+    }
+  }
+
+  console.log('检测filterData--:', data, '结果', isHas);
+  
+  return isHas;
+}
+
+// 重置数据
+const resetData = () => {
+  parent = '';
+  content = '';
+  cachContent = '';  
 }
 getFile();
